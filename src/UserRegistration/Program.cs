ec9ac4457500d4e04c9fd0d4a031041622de867e
Edward Wilson <edward.wilson@live.com>
2017-12-20 21:14:50 +0000

    Removed unused using statment

--- src/UserRegistration/Program.cs
+++ src/UserRegistration/Program.cs
@@ -3,7 +3,6 @@
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
     using Serilog;
     using System;
     using System.IO;
