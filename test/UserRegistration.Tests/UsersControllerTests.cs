b46bd8ca5a47327acdb513c125e5aab66d4998e5
Edward Wilson <edward.wilson@live.com>
2017-12-21 15:51:51 +0000

    Added unit test for model validation

--- test/UserRegistration.Tests/UsersControllerTests.cs
+++ test/UserRegistration.Tests/UsersControllerTests.cs
@@ -1,8 +1,10 @@
 namespace UserRegistration.Tests
 {
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
     using UserRegistration.Controllers;
     using UserRegistration.Models;
+    using UserRegistration.Services;
     using Xunit;
 
     public class UsersControllerTests
@@ -11,7 +13,7 @@ namespace UserRegistration.Tests
         public void Register_ReturnsAViewResult_WithAUserViewModel()
         {
             // Arrange
-            var controller = new UsersController(null);
+            var controller = new UsersController(null, null);
 
             // Act
             var result = controller.Register();
@@ -19,6 +21,23 @@ namespace UserRegistration.Tests
             // Assert
             Assert.IsType<ViewResult>(result);
             Assert.IsAssignableFrom<UserViewModel>(((ViewResult) result).ViewData.Model);
+        }
+
+        [Fact]
+        public void Register_ReturnsModelStateError_WithInvalidModel()
+        {
+            // Arrange
+            var userStoreMoq = new Moq.Mock<IUserStore>();
+            var loggerMoq = new Moq.Mock<ILogger<UsersController>>();
+
+            var controller = new UsersController(userStoreMoq.Object, loggerMoq.Object);
+
+            // Act
+            var result = controller.Register(new UserViewModel{ Password = "MyPassword1"});
+
+            // Assert
+            // This doesn't seem to work; could be a bug with .net core
+            Assert.False(((ViewResult)result).ViewData.ModelState.IsValid);
         }
     }
 }
