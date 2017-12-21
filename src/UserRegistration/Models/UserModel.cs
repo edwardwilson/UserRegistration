f079a4cd50335283598f93c557045b5aba6c3531
Edward Wilson <edward.wilson@live.com>
2017-12-21 13:29:12 +0000

    Setup validation rules and added data model

---
+++ src/UserRegistration/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace UserRegistration.Models
+{
+    public class UserModel
+    {
+        public UserModel(string emailAddress, string password)
+        {
+            EmailAddress = emailAddress;
+            Password = password;
+        }
+
+        public string EmailAddress { get; }
+        public string Password { get; }
+    }
+}
--- src/UserRegistration/Models/UserViewModel.cs
+++ src/UserRegistration/Models/UserViewModel.cs
@@ -1,9 +1,15 @@
 ﻿namespace UserRegistration.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class UserViewModel
     {
+        [Required]
+        [EmailAddress]
         public string EmailAddress { get; set; }
 
+        [Required]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,64}$")]
         public string Password { get; set; }
     }
 }
